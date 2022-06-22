70dce93f5694523bc021176764ed12e07e2a66ef
devgustavovasquez <gustavovasquez2002@gmail.com>
2022-06-21 21:55:43 -0300

    feat: create base components to page Event

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { gql, useQuery } from "@apollo/client"
 
+import { Event } from "./pages/Event";
+
 const GET_LESSONS_QUERY = gql`
   query {
     lessons {
@@ -19,16 +21,8 @@ interface Lesson {
 }
 
 function App() {
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
-  console.log(data)
-
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li key={lesson.id}>{lesson.title}</li>
-      })}
-    </ul>
+    <Event />
   )
 }
 
